e3feb19b63153a404c46993d86202ec3cb640d4f
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-06-09 20:53:48 +0530

    Add files via upload

---
+++ Invers_Num_Permid.java
@@ -0,0 +1,18 @@
+class Invers_Num_Permid
+{
+public static void main(String[] args)
+{
+for(int i=1;i<=5;i++)
+{
+  for(int j = 1; j<i;j++)
+{
+  System.out.print(" ");
+}
+    for(int n=5;n>=i;n--)
+{
+      System.out.print(i+" "); 
+}
+          System.out.println();
+}
+}
+} 
